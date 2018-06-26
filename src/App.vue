8960d26db54b91efb25371a324234c2087b3586a
Thomas Yager-Madden <10351894+tym-oao@users.noreply.github.com>
2018-06-26 09:07:38 -0500

    EUChart component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <EUChart />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EUChart from './components/EUChart.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    EUChart
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/EUChart.vue
@@ -0,0 +1,70 @@
+<template> 
+  <div class="ct-chart">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+import Chartist from 'chartist'
+import groupBy from 'lodash.groupby'
+import moment from 'moment'
+
+export default {
+  name: 'EUChart',
+  data() {
+    return {
+      message: 'Loading...',
+      series: [],
+      labels: [],
+      data: {}
+    }
+  },
+  methods: {
+    populateChart() {
+      // JSON data from Nerium http://nerium:8080/v1/alpha-eu-revenue-monthly-summary/
+      fetch('./alpha-eu.json')
+      .then(response => response.json())
+      // Separate month_rev to series by EU/Non-EU
+      .then(data => this.data = groupBy(data.data, month => month.european_union))
+      .then(series => this.series = [
+        series.EU.map(rev => rev.monthly_rev), 
+        series['Non-EU'].map(rev => rev.monthly_rev)
+        ]
+      )
+      // Set bar labels to months
+      .then(() => this.labels = this.data.EU.map(month => month.month))
+      // Provision Chartist stacked bar chart
+      .then(() => {
+        console.log([this.labels, this.series])
+        var data = { 
+          labels: this.labels, 
+          series: this.series 
+          }
+        var options = {
+          axisX: { 
+            labelInterpolationFnc: function(value) {
+              return (value / 1000) + 'k' 
+            }
+          },
+          axisY: { 
+            labelInterpolationFnc: function(value) { 
+              return moment(value).format('MMM YYYY') 
+            }
+          },
+          height: '500px',
+          horizontalBars: true,
+          reverseData: true,
+          stackBars: true, 
+          width: '67%'
+        } 
+        new Chartist.Bar('.ct-chart', data, options)
+        })
+      // Void the loading message
+      .then(() => this.message = null)
+    }
+  },
+  mounted () {
+    this.populateChart()
+  } 
+}
+</script>
