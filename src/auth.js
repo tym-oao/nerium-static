a34fa63bc6ae1980107881a9e756efca7fd87d11
Thomas Yager-Madden <10351894+tym-oao@users.noreply.github.com>
2018-06-28 09:32:00 -0500

    New redirectUri

--- src/auth.js
+++ src/auth.js
@@ -5,7 +5,8 @@ import Vue from 'vue'
 let webAuth = new auth0.WebAuth({
   domain: 'tym-oao.auth0.com',
   clientID: '3WW4lg3F0kOBxxRABPvhcW3eGJMKrMIu',
-  redirectUri: 'https://pedantic-hugle-486026.netlify.com/callback',
+  // TODO: get redirectUri from environment variable
+  redirectUri: 'https://nerium-static.ty-m.pw/callback',
   // we will use the api/v2/ to access the user information as payload
   audience: 'https://tym-oao.auth0.com/api/v2/',
   responseType: 'token id_token',
