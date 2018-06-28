2009dff052223b2f74e4801ced8970ee01e36847
Thomas Yager-Madden <10351894+tym-oao@users.noreply.github.com>
2018-06-28 15:17:17 -0500

    Router and Auth0 (#1)
    * Add router, auth, and auth callback views
    
    Provides Auth0 authentication and router guard to limit access to report view to logged in users only. Example report is using my Auth0 account, which includes a rule to deny access to users except from adops.com email domain.

---
+++ src/auth.js
@@ -0,0 +1,96 @@
+import auth0 from 'auth0-js'
+import Vue from 'vue'
+
+const url = new URL(window.location.href)
+
+// TODO: Separate config file for this.
+const webAuth = new auth0.WebAuth({
+  domain: 'tym-oao.auth0.com',
+  clientID: '3WW4lg3F0kOBxxRABPvhcW3eGJMKrMIu',
+  redirectUri: url.protocol + '//' + url.host + '/callback',
+  // redirectUri: 'https://nerium-static.ty-m.pw/callback',
+  // we will use the api/v2/ to access the user information as payload
+  audience: 'https://tym-oao.auth0.com/api/v2/',
+  responseType: 'token id_token',
+  scope: 'openid profile email'
+})
+
+const auth = new Vue({
+  computed: {
+    token: {
+      get: function() {
+        return localStorage.getItem('id_token')
+      },
+      set: function(id_token) {
+        localStorage.setItem('id_token', id_token)
+      }
+    },
+    accessToken: {
+      get: function() {
+        return localStorage.getItem('access_token')
+      },
+      set: function(accessToken) {
+        localStorage.setItem('access_token', accessToken)
+      }
+    },
+    expiresAt: {
+      get: function() {
+        return localStorage.getItem('expires_at')
+      },
+      set: function(expiresIn) {
+        let expiresAt = JSON.stringify(expiresIn * 1000 + new Date().getTime())
+        localStorage.setItem('expires_at', expiresAt)
+      }
+    },
+    user: {
+      get: function() {
+        return JSON.parse(localStorage.getItem('user'))
+      },
+      set: function(user) {
+        localStorage.setItem('user', JSON.stringify(user))
+      }
+    }
+  },
+  methods: {
+    login() {
+      webAuth.authorize()
+    },
+    logout() {
+      return new Promise((resolve, reject) => { 
+        localStorage.removeItem('access_token')
+        localStorage.removeItem('id_token')
+        localStorage.removeItem('expires_at')
+        localStorage.removeItem('user')
+        this.login()
+      })
+    },
+    isAuthenticated() {
+      return new Date().getTime() < this.expiresAt
+    },
+    handleAuthentication() {
+      return new Promise((resolve, reject) => {  
+        webAuth.parseHash((err, authResult) => {
+          const valid = authResult && authResult.accessToken && authResult.idToken
+          if (valid) {
+            this.expiresAt = authResult.expiresIn
+            this.accessToken = authResult.accessToken
+            this.token = authResult.idToken
+            this.user = authResult.idTokenPayload
+            resolve()
+
+          } else if (err) {
+            this.logout()
+            reject(err)
+          }
+
+        })
+      })
+    }
+  }
+})
+
+export default {
+  install: function(Vue) {
+    Vue.prototype.$auth = auth
+  }
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>
-    <h1 class="title">{{ title }}</h1>
-    <div class="ct-chart ct-golden-section">
-      {{ message }}
-    </div>
-  </div>
+  <main class="main">
+    <section class="section">
+        <div class="columns is-centered">
+          <div class="column">
+            <h1 class="title">{{ title }}</h1>
+            <div class="ct-chart ct-golden-section">
+              {{ message }}
+            </div>
+            <div class="section">
+              <p class="has-text-right">
+                <button type="button" class="button is-normal is-link" @click="$auth.logout()">Logout</button>
+              </p>
+            </div>
+          </div>
+        </div>
+    </section>
+  </main>
 </template>
 
 <script>
@@ -13,7 +24,6 @@ import groupBy from 'lodash.groupby'
 import moment from 'moment'
 
 export default {
-  name: 'EUChart',
   data() {
     return {
       title: 'Alpha Monthly Revenue, EU vs. Non-EU',
@@ -39,7 +49,6 @@ export default {
       .then(() => this.labels = this.data.EU.map(month => month.month))
       // Provision Chartist stacked bar chart
       .then(() => {
-        console.log([this.labels, this.series])
         var data = { 
           labels: this.labels, 
           series: this.series 
