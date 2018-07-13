d7fe7d731148669bfc4cacdaf1c2d94e53798093
Thomas Yager-Madden <10351894+tym-oao@users.noreply.github.com>
2018-07-13 15:06:32 -0500

    add sqlite fallback to cannery-result

--- src/scripts/cannery-result.js
+++ src/scripts/cannery-result.js
@@ -2,38 +2,66 @@
 require('dotenv').config()
 
 // TODO: `fetch` instead of fs so query can be remote as well as local
+const path = require('path')
 const fs = require('fs')
 
 // connect to $DATABASE_URL
 const db = require('pg-db')()
 // import config object
 const CANNERY_CONFIG = require('./cannery-config')
+const query = CANNERY_CONFIG.queryName
+const params = CANNERY_CONFIG.queryParams || {}
+const output = CANNERY_CONFIG.outputName || CANNERY_CONFIG.queryName
+
+
+function writeOutput(rows) {
+  fs.writeFile(
+    path.resolve(__dirname, '../../public/') + path.sep + output + '.json',
+    JSON.stringify(rows),
+    () => {
+      console.log('done!')
+    })
+}
 
 // read query from file
 // TODO: 
 //   - frontmatter parsing
 //   - export this as function for serverless use
 fs.readFile(
-  './queries/' + CANNERY_CONFIG.queryName + '.sql',
+  path.resolve(__dirname, './queries/') + path.sep + query + '.sql',
   'utf8',
   // submit query to db
   (err, data) => {
     if (err) throw err
-    const params = CANNERY_CONFIG.queryParams || {}
     db.query(
       data, 
       params,
       // write results to JSON file in /public
       // TODO: include supplemental metadata/additional formatting options in output
       (err, rows) => {
-        if (err) throw err
-        const output = CANNERY_CONFIG.outputName || CANNERY_CONFIG.queryName
-        fs.writeFile(
-          '../../public/' + output +'.json',
-          JSON.stringify(rows),
-          () => {
-            console.log('done!')
-      })
+        if (err) {
+          // fallback to sqlite for build/test without Postgres connection
+          console.log('Warning: no Postgres connection, trying sqlite...')
+          const sqlite3 = require('sqlite3')
+          lite = new sqlite3.Database(':memory:')
+          // Prepend `:` to parameter keys for sqlite 😠
+          liteParams = Object.assign({}, ...Object.keys(params).map(key => {
+            var newKey = ':' + key
+            return {
+              [newKey]: params[key]
+            }
+          }))
+          lite.all(
+            data,
+            liteParams,
+            function (err, rows) {
+              if (err) throw err
+              writeOutput(rows)
+            }
+          )
+        } else {
+          writeOutput(rows)
+        }
     })
   }
 )
